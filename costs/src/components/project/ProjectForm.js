2df56fab44d677e3fc86292edab87c4d23af2fb2
Higor Romano <higorjromano@gmail.com>
2026-01-30 22:26:04 -0300

    componentização pt 2
    fiz o componente select e submitbutton e seus estilos na pasta form, e importei eles no componente projectform.

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,20 +1,16 @@
 import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
 
 import styles from './ProjectForm.module.css'
 
-function ProjectForm() {
+function ProjectForm({btnText}) {
     return (
         <form className={styles.form}>
            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
            <Input type="number" text="Orçamento total" name="budget" placeholder="Insira o orçamento total" />
-          <div>
-             <select name="category_id">
-                <option dis>Selecione uma categoria</option>
-             </select>
-          </div>
-          <div>
-             <input type="submit" value="Criar Projeto" />
-          </div>
+           <Select name="category_id" text="Selecione a categoria" />
+           <SubmitButton text={btnText}/>
         </form>
     )
 }
