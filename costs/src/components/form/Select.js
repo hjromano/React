2ccfd13caa5f4b1f48c7ac50a642b60704157cef
Higor Romano <higorjromano@gmail.com>
2026-02-01 13:16:47 -0300

    Inserindo dados no banco via API
    aprendi a enviar os dados  para o db.json pelo site que estamos criando

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -1,11 +1,11 @@
 import styles from './Select.module.css'
 
-function Select({ text, name, options, handleOnChange, value}) {
+function Select({ text, name, options, handleChange, value}) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}</label>
-            <select name={name} id={name}> 
-                <option>Selecione uma opção</option>
+            <select name={name} id={name} onChange={handleChange} value={value || ''}> 
+                <option value="">Selecione uma opção</option>
                 {options.map((option) => (
                     <option value={option.id} key={option.id}>{option.name}</option>
                 ))}
--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -5,9 +5,10 @@ import SubmitButton from '../form/SubmitButton'
 
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
 
     const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
 
     useEffect(() => {
         fetch('http://localhost:5000/categories', {
@@ -21,11 +22,32 @@ function ProjectForm({btnText}) {
     .catch((err) => console.log(err))
     }, [])
 
+    const submit = (e) => {
+        e.preventDefault()
+        // console.log(project)
+        handleSubmit(project)
+    }
+
+    function handleChange(e) {
+        setProject({ ...project, [e.target.name]: e.target.value })
+
+    }
+
+    function handleCategory(e) {
+        setProject({
+            ...project,
+            category: {
+                id: e.target.value,
+                name: e.target.options[e.target.selectedIndex].text
+            }
+        })
+    }
+
     return (
-        <form className={styles.form}>
-           <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
-           <Input type="number" text="Orçamento total" name="budget" placeholder="Insira o orçamento total" />
-           <Select name="category_id" text="Selecione a categoria" options={categories} />
+        <form onSubmit={submit} className={styles.form}>
+           <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" handleChange={handleChange} value={project.name ? project.name : ''} />
+           <Input type="number" text="Orçamento total" name="budget" placeholder="Insira o orçamento total" handleChange={handleChange} value={project.budget ? project.budget : ''} />
+           <Select name="category_id" text="Selecione a categoria" options={categories} handleChange={handleCategory} value={project.category ? project.category.id : ''} />
            <SubmitButton text={btnText}/>
         </form>
     )
