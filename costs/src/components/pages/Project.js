d15f51861a27ca03f51062b6f1fee500c83b25be
Higor Romano <higorjromano@gmail.com>
2026-02-23 23:10:20 -0300

    Removendo serviços

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -107,7 +107,31 @@ function Project() {
         .catch(err => console.log(err))
     }
 
-    function removeService() {}
+    function removeService(id, cost) {
+        setMessage('')
+
+        const servicesUpdated = project.services.filter((service) => service.id !== id)
+
+        const projectUpdated = project
+        projectUpdated.services = servicesUpdated
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(projectUpdated),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProject(projectUpdated)
+            setServices(servicesUpdated)
+            setMessage('Serviço removido com sucesso!')
+            setType('success')
+        })
+        .catch(err => console.log(err))
+    }
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
--- costs/src/components/service/ServiceCard.js
+++ costs/src/components/service/ServiceCard.js
@@ -4,7 +4,11 @@ import { BsFillTrashFill } from 'react-icons/bs'
 
 function ServiceCard({ id, name, cost, description, handleRemove }) {
     
-    const remove = (e) => {}
+    const remove = (e) => {
+        e.preventDefault()
+        handleRemove(id, cost)
+    }
+
     return (
         <div className={styles.project_card}>
             <h4>{name}</h4>
